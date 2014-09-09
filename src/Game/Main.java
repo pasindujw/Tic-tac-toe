1ae532b3ae880f1dd12dd41d674b195a492955f6
pasindujw <pasinduonline@gmail.com>
2014-09-10 00:58:20 +0530

    uploading folders

---
+++ src/Game/Main.java
@@ -0,0 +1,31 @@
+package Game;
+
+import javax.swing.JOptionPane;
+
+public class Main {
+
+    public static TicTakUI board = new TicTakUI();
+    public static void main(String[] args) {
+        if(board.isVisible()==false){
+            board.setVisible(true);
+        }
+        
+    }
+    
+    public static void checkforwin(){
+        if(board.jButton1.getText().equals("X")){
+            if(board.jButton5.getText().equals("X")){
+                
+                if(board.jButton9.getText().equals("X")){
+                    JOptionPane.showMessageDialog(board, "Your win..", "Win", JOptionPane.OK_OPTION);
+      
+                }
+            }
+                    
+            
+            
+        }
+    }
+    
+}
+ 
